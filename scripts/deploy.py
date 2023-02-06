e6459f2affb7655a662cc9a7b9dd94e78d0d8839
harshhvv <76173453+harshhvv@users.noreply.github.com>
2023-02-06 15:14:32 +0530

    final

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,15 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
+
+
+def get_account():
+    if network.show_active == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def deploy_simple_storage():
-    account = accounts[0]
+    account = get_account()
     # account = accounts.add(config["wallets"]["from_key"])
 
     simple_storage = SimpleStorage.deploy({"from": account})
